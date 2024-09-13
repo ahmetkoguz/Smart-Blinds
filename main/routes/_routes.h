14c58353fb5ef74df01469aaca82e6350b6c67e9
Ahmet Oguz <ahmetkerimoguz@gmail.com>
2024-09-13 16:23:20 -0400

    temp

---
+++ main/routes/_routes.h
@@ -0,0 +1,21 @@
+#ifndef routes_h
+#define routes_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+// #include "cJSON.h"
+// #include "esp_log.h"
+#include "esp_http_server.h"
+// #include "../app.h"
+
+/******************** init function ****************/
+void init_btn(void);
+void init_led(void);
+
+/******************** http callbacks ****************/
+esp_err_t on_toggle_led_url(httpd_req_t *req);
+/**************************************************/
+void toggle_led(bool is_on);
+
+#endif
