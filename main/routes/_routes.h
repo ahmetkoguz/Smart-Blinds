b93f43c198ff93ec575934cfd12b58b3c433f33c
FangLing Zhang <89488428+El-Psy-Congro@users.noreply.github.com>
2024-10-08 17:59:35 -0400

    Update _routes.h

--- main/routes/_routes.h
+++ main/routes/_routes.h
@@ -1,5 +1,5 @@
-#ifndef routes_h
-#define routes_h
+#ifndef ROUTES_H
+#define ROUTES_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +9,7 @@
 #include "esp_http_server.h"
 #include "../app.h"
 
-/******************** init function ****************/
+/******************** Init Functions ****************/
 void init_btn(void);
 void init_servo(void);
 void init_lower(void);
@@ -18,7 +18,15 @@ void init_raise(void);
 void adc_init_lower(void);
 void adc_init_raise(void);
 
-/******************** http callbacks ****************/
+/******************** Interrupt Setup Functions ****************/
+void init_reed_sensor_interrupt_lower(void);
+void init_reed_sensor_interrupt_raise(void);
+
+/******************** Timer Setup Functions ****************/
+void setup_adc_timer_lower(void);
+void setup_adc_timer_raise(void);
+
+/******************** HTTP Callbacks ****************/
 esp_err_t on_raise(httpd_req_t *req);
 esp_err_t on_lower(httpd_req_t *req);
 esp_err_t on_stop(httpd_req_t *req);
@@ -27,9 +35,10 @@ esp_err_t on_hello_world(httpd_req_t *req);
 esp_err_t on_magnetometer(httpd_req_t *req);
 esp_err_t on_web_socket_btn_push_url(httpd_req_t *req);
 esp_err_t on_servo_url(httpd_req_t *req);
-/**************************************************/
+
+/******************** Toggle Functions ****************/
 void toggle_lower(bool is_on);
 void toggle_raise(bool is_on);
 void toggle_stop(bool is_on);
 
-#endif
+#endif // ROUTES_H
