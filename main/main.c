ace2724e3a9e85e5da519df43f366a1d225362c6
FangLing Zhang <89488428+El-Psy-Congro@users.noreply.github.com>
2024-10-11 14:28:44 -0400

    Update main.c

--- main/main.c
+++ main/main.c
@@ -290,11 +290,11 @@ void app_main(void)
   ESP_ERROR_CHECK(wifi_connect_sta(SSID, PASS, 10000));
   // wifi_connect_ap("esp32ap", "password");
 
-  while (1) {
-        button_press();
+  // while (1) {
+  //       button_press();
       
-        vTaskDelay(pdMS_TO_TICKS(100));  // Adjust delay as needed
-    }
+  //       vTaskDelay(pdMS_TO_TICKS(100));  // Adjust delay as needed
+  //   }
 
   start_mdns_service();
   init_server();
